4f9803e880faa4369f48ed2576b84ade54ce3967
Julia Gonzalez <juliagonzalezmoreira@gmail.com>
2025-03-21 08:07:24 -0300

    feat: create home screen, project component and routes

--- app/App.tsx
+++ app/App.tsx
@@ -1,14 +1,29 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import Home from './src/pages/home/Home';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Historico from './src/pages/historico/Historico';
+import RegistroDespesa from './src/pages/registroDespesa/RegistroDespesa';
+import Perfil from './src/pages/perfil/Perfil';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="RegistroDespesa" component={RegistroDespesa} />
+        <Stack.Screen name="Historico" component={Historico} />
+        <Stack.Screen name="Perfil" component={Perfil} />
+        {/* Adicionar rota para página do projeto*/}
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
