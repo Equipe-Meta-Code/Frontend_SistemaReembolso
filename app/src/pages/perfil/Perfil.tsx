9af64261c334c0accdff645f9497d8ae07f7d624
Pedro <pedro.ribeiro33@fatec.sp.gov.br>
2025-03-25 21:53:23 -0300

    feat: button menu

--- app/src/pages/perfil/Perfil.tsx
+++ app/src/pages/perfil/Perfil.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Switch  } from 'react-native';
 import { style } from "./styles";
 import Indicadores from '../../components/perfil/Indicadores';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
 
 const Perfil = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(previousState => !previousState);
+    };
+
     return (
         <View style={style.container}>
             <View style={style.corTopo}></View>
@@ -18,14 +27,32 @@ const Perfil = () => {
                 Pedro Henrique Ribeiro
                 </Text>
 
-                <View style={style.departamento}>
-                    <Text style={style.departamentoTexto}>
-                        Logística
-                    </Text>        
-                </View>
+                <Text style={style.emailFuncionario}>
+                    pedro09.2004@gmail.com
+                </Text>        
+               
+            </View>
+            <View style={style.divisor} />
+
+            <View style={style.containerMostradores}>
+                <Indicadores titulo="Projetos" quantia={"2"} />
+                <Indicadores titulo="Pendente" quantia={"R$4K"} />
+            </View>
+            <View style={style.subtituloContainer}>
+
+                <Text style={style.subtitulo}>Geral</Text>
             </View>
 
-            <View style={style.botoes}>
+            <View style={style.containerBotoes}>
+                <TouchableOpacity style={style.botao} onPress={() => alert("Botão Personal Info clicado")}>
+                    <Text style={style.textoBotao}>Informações pessoais</Text>
+                    <Ionicons name="chevron-forward" size={20} color="#000" />
+                </TouchableOpacity>
+
+                </View>
+                
+            </View>
+
 
                 <View style={style.editContainer}>
                     <Text style={style.textoBotao}>
@@ -38,11 +65,7 @@ const Perfil = () => {
                     Alterar Senhaa
                 </Text>
                 </View>
-            </View>
-            <View style={style.containerMostradores}>
-                <Indicadores titulo="Projetos" quantia={"2"} />
-                <Indicadores titulo="Pendente" quantia={"R$4K"} />
-            </View>
+            </View> */}
         </View>
     );
 };
