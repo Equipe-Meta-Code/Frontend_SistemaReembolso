b2d09c5841c70a4b80573deec776fbf834c89153
mluizaguede <mluizaguedessilva@gmail.com>
2025-04-26 14:28:50 -0300

    fix: restore accidentally deleted code

--- app/src/pages/registroDespesa/RegistroDespesa.tsx
+++ app/src/pages/registroDespesa/RegistroDespesa.tsx
@@ -16,6 +16,7 @@ const GAS_PRICE = 6.20; // preço fixo da gasolina
 
 const RegistroDespesa = () => {
   const [error, setError] = useState("");
+  const [pacoteError, setPacoteError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [category, setCategory] = useState("");
   const [categoriesByProject, setCategoriesByProject] = useState<{ [key: string]: { label: string; value: string }[] }>({});
@@ -62,6 +63,7 @@ const RegistroDespesa = () => {
     nome: string;
     projetoId: string;
     userId: string;
+    status: string;
   };
 
   const fetchData = async () => {
@@ -69,17 +71,24 @@ const RegistroDespesa = () => {
       let response = await api.get('/projeto');
 
       const projetos = response.data;
-      setAllProjects(projetos);
+      const userId = Number(user?.userId);
+      const userProjects = projetos.filter((project: any) =>
+        project.funcionarios?.some((func: any) => func.userId === userId)
+      );          
+      console.log('projetos', projetos)
+      console.log('user projetos', userProjects)
+
+      setAllProjects(userProjects);
 
       // Transforma a lista de projetos para o dropdown
-      const formattedProjects = projetos.map((projeto: Project) => ({
+      const formattedProjects = userProjects.map((projeto: Project) => ({
         label: projeto.nome,
         value: projeto.projetoId.toString(), // Certifique-se de que é string
       }));
 
       // Cria um objeto com categorias agrupadas por projeto
       const categoriasPorProjeto: { [key: string]: { label: string; value: string }[] } = {};
-
+      
       projetos.forEach((projeto: any) => {
         const categoriasFormatadas = projeto.categorias.map((cat: Category) => ({
           label: cat.nome,
@@ -87,7 +96,7 @@ const RegistroDespesa = () => {
         }));
         categoriasPorProjeto[projeto.projetoId.toString()] = categoriasFormatadas;
       });
-
+   
       setProjects(formattedProjects);
       setCategoriesByProject(categoriasPorProjeto);
     } catch (error) {
@@ -101,52 +110,50 @@ const RegistroDespesa = () => {
       const pacotes: Pacote[] = response.data;
       console.log(pacotes)
 
-      // Filtra os pacotes que pertencem ao usuário
-      const pacotesByUser = pacotes.filter(
-        (pacote) => pacote.userId.toString() === user?.userId.toString()
+      // Filtra os pacotes
+      const pacotesFiltrados = pacotes.filter(
+        (pacote) =>
+          pacote.userId.toString() === user?.userId.toString() &&
+          pacote.projetoId.toString() === projetoId &&
+          pacote.status === "rascunho"
       );
-
-      // Filtra os pacotes que pertencem ao projeto selecionado
-      const pacotesFiltrados = pacotesByUser.filter(
-        (pacote) => pacote.projetoId.toString() === projetoId
-      );
-
+  
       // Formata os pacotes filtrados para o dropdown
       const pacotesFormatados = pacotesFiltrados.map((pacote) => ({
         label: pacote.nome,
         value: pacote.pacoteId.toString(),
       }));
-
-      setPacotes(pacotesFormatados);
-      console.log('formatados:', pacotesFormatados)
+  
+      setPacotes(pacotesFormatados); 
+      console.log('formatados:',pacotesFormatados)
     } catch (error) {
       console.error("Erro ao buscar pacotes:", error);
     }
-  };
-
+  };  
+  
   useEffect(() => {
     if (selectedProject && category) {
       const fetchDataDespesas = async () => {
         try {
           let response = await api.get('/despesa');
-
+  
           const despesas = response.data;
-
+  
           const despesasFiltradas = despesas.filter(
             (despesa: any) =>
               despesa.projetoId.toString() === selectedProject &&
               despesa.categoria.toString() === category
           );
-
+  
           const total = despesasFiltradas.reduce((acc: number, curr: any) => {
             return acc + parseFloat(curr.valor_gasto);
           }, 0);
-
+  
           setTotalGastoCategoria(total);
         } catch (error) {
           console.error("Erro ao buscar despesas por categoria:", error);
         }
-      };
+      };     
       fetchDataDespesas();
     }
   }, [selectedProject, category]);
@@ -158,6 +165,15 @@ const RegistroDespesa = () => {
 
     if (!newPacoteName || !selectedProject) {
       setError("Informe o nome do pacote.");
+      return;
+    }
+
+    const pacoteExistente = pacotes.find(
+      (p) => p.label.trim().toLowerCase() === newPacoteName.trim().toLowerCase()
+    );
+
+    if (pacoteExistente) {
+      setPacoteError("Já existe um pacote com esse nome neste projeto.");
       return;
     }
 
@@ -367,7 +383,10 @@ const RegistroDespesa = () => {
 
           {/* Se o usuário quiser criar um novo pacote */}
           {!creatingPacote ? (
-            <TouchableOpacity onPress={() => setCreatingPacote(true)}>
+            <TouchableOpacity onPress={() => {
+                setCreatingPacote(true);
+                setPacoteError("");
+            }}>
               <Text style={styles.link}> + Criar novo pacote </Text>
             </TouchableOpacity>
           ) : (
@@ -378,11 +397,15 @@ const RegistroDespesa = () => {
                 onChangeText={setNewPacoteName}
                 style={styles.inputNome}
               />
-
+              
               <TouchableOpacity style={styles.smallButton} onPress={handleCreatePacote}>
                 <Text style={styles.buttonText}> Criar Pacote </Text>
               </TouchableOpacity>
-
+              {pacoteError && (
+                <Text style={[styles.pacoteErrorMessage, { marginTop: 4 }]}>
+                  {pacoteError}
+                </Text>)}
+              
               <TouchableOpacity onPress={() => setCreatingPacote(false)}>
                 <Text style={styles.link}> Cancelar </Text>
               </TouchableOpacity>
